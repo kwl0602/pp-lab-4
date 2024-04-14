e6efa54ba355c804bbd5a6e22a2c3c07978e4367
DawidA <78934924+kwl0602@users.noreply.github.com>
2024-04-14 12:08:16 +0200

    Update Company.java

--- Company.java
+++ Company.java
@@ -6,7 +6,7 @@ public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
 
-        employees[0] = new Manager("Marcin Kasek", 85000, 2);
+        employees[0] = new Manager("Marcin Kasek", 85000, 0);
         employees[1] = new Worker("Kamil Vas", 67000, "Programista");
         employees[2] = new Employee("Aleksandra Kas", 60000);
         employees[3] = new Manager("Maciek Das", 55000, 5);
