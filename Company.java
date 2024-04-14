6405290056bf66b759d9d61bb44be60ab1e60264
unknown <daavwid2001@gmail.com>
2024-04-14 12:25:28 +0200

    zad3

--- Company.java
+++ Company.java
@@ -6,11 +6,24 @@ public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
 
-        employees[0] = new Manager("Marcin Kasek", 85000, 2);
+        employees[0] = new Manager("Marcin Kasek", 85000, 0);
         employees[1] = new Worker("Kamil Vas", 67000, "Programista");
         employees[2] = new Employee("Aleksandra Kas", 60000);
         employees[3] = new Manager("Maciek Das", 55000, 5);
         employees[4] = new Worker("Lucek Mas", 110000, "Informatyk");
+
+        int nonManagerCount = 0;
+        for (Employee emp : employees) {
+            if (!(emp instanceof Manager)) {
+                nonManagerCount++;
+            }
+        }
+
+        if (employees[0] instanceof Manager) {
+            ((Manager) employees[0]).setNumberOfSubordinates(nonManagerCount);
+        }
+
+        employees[0].setSalary(7500);
 
         for (Employee emp : employees) {
             System.out.println(emp);
